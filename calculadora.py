54ea648f156ba18c21f59fedd7f6160f1adc0896
god-irzin <iranjunior245@gmail.com>
2024-10-30 13:47:25 -0300

    Add files via upload
    2024-10-30

---
+++ calculadora.py
@@ -0,0 +1,22 @@
+# contador = 0
+# contador = contador + 1
+# print(contador + 1)
+# print(contador)
+
+x = float(input("informe o primeiro valor:"))
+y = float(input("informe o segundo valor:"))
+
+z = int(x) + int(y)
+print(f"A soma de {x} + {y} = {z}")
+
+z = int(x) * int(y)
+print(f"A multiplicação de {x} * {y} = {z}")
+
+z = int(x) - int(y)
+print(f"A subtração de {x} - {y} = {z}")
+
+z = int(x) / int(y)
+print(f"A divisão de {x} / {y} = {z}")
+
+z = int(x) % int(y)
+print(f"O resto de {x} % {y} = {z}")
