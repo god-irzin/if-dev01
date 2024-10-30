ff1a1f5720b4ac2e97761141e170ba7b695ed3de
god-irzin <iranjunior245@gmail.com>
2024-10-30 13:47:52 -0300

    Add files via upload
    2024-10-30

---
+++ string-methods.py
@@ -0,0 +1,22 @@
+nome = input("digite seu nome:")
+# print(f"olá,{nome.lower()}")
+
+# print (f"olá, {nome.upper()}")
+# print(f"olá,{nome.capitalize()}")
+
+#  print(f"olá,{nome.title()}")
+
+# print(f"olá,{nome.lstrip()}")
+# print(f"olá,{nome.strip()}")
+# print(f"olá,{nome.rsplip()}")
+
+# print(f"olá,{nome.split()}")
+# nome = " ".join(nome)
+# nome , sobrenome = nome.split()
+# nome_completo = nome + " " + sobrenome
+# print(f"olá, {nome_completo.title()}")
+
+frase = "Olá mundo!"
+#print(frase.replace("Mundo", nome))
+
+print(frase.lower().find(nome.lower()))
